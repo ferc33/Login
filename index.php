f4495a59cd33c2c283d4cdde19e225c94207183e
Fernando C <fercassera@gmail.com>
2022-05-13 21:48:46 -0300

    Login Creado

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 
-/*#EL INDEX: En el mostraremos la salida de las vistas al usuario
-y tambien a traves de el enviaremos la distintas acciones que 
-el usuario envie al controlador*/
+#EL INDEX: En él mostraremos la salida de las vistas al usuario y también a traves de él enviaremos las distintas acciones que el usuario envíe al controlador.
 
-/**/
-require_once "modelo/conexion.php";
-require_once "modelo/formulario.modelo.php";
+#require() establece que el código del archivo invocado es requerido, es decir, obligatorio para el funcionamiento del programa. Por ello, si el archivo especificado en la función require() no se encuentra saltará un error “PHP Fatal error” y el programa PHP se detendrá.
 
-require_once "controlador/plantilla.controlador.php";
-require_once "controlador/formulario.controlador.php";
+#La versión require_once() funcionan de la misma forma que sus respectivo, salvo que, al utilizar la versión _once, se impide la carga de un mismo archivo más de una vez.
+#Si requerimos el mismo código más de una vez corremos el riesgo de redeclaraciones de variables, funciones o clases. 
 
-$conexion = Conexion::conectar(); //metodo static
+require_once "controladores/plantilla.controlador.php";
+
+require_once "controladores/formularios.controlador.php";
+require_once "modelos/formularios.modelo.php";
+
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrTraerPlantilla();
+
+
--- vista/paginas/error404.php
+++
@@ -1,5 +0,0 @@
-<div class = "text-center">
-<h1>Error 404!</h1>
-<p>Página no encontrada </p>
-</div>
-
